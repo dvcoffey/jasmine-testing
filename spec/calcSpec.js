a2e833b936a3c2a403fb1d69106a4fe8116dafc7
dvcoffey <dvcoffey@gmail.com>
2020-09-04 15:17:19 +0000

    scripts updated to utilize Spyon alert functio

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -7,7 +7,9 @@ describe("Calculator", function(){
             expect(addition(7,19)).toBe(26);
         })
         it("should return an error if we don't supply two numbers", function(){
-            expect(addition("Hitchikers", "Guide")).toBe("Error!")
+            spyOn(window, "alert");
+            addition("Hitchhiker's", "Guide");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
         })
     });
 });
